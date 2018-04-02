c5a98275a558c051583c107683ba478e2c21717e
harrinp <30451999+harrinp@users.noreply.github.com>
2018-04-02 18:50:41 -0400

    Add files via upload

---
+++ Config.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <unistd.h>
+
+// MYSQL parameters
+static const std::string URL = "tcp://127.0.0.1:3306";
+static const std::string USER = "root";
+static const std::string PASSWORD = "";
+
+// OANDA parameters
+static const std::string ACCOUNT_ID = "101-001-6844455-001";
+static const std::string ACCESS_TOKEN = "3481fa8462af186c57b6d6d03a37a0fd-602c4ee66c827bcb87fabddd1da5c094";
+
+#endif
