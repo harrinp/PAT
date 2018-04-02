dcbfee4bc0d2b3716eae0d4e849ffdf7c881a7be
harrinp <30451999+harrinp@users.noreply.github.com>
2018-04-02 18:52:38 -0400

    Update Config.hpp

--- Config.hpp
+++ Config.hpp
@@ -6,10 +6,10 @@
 // MYSQL parameters
 static const std::string URL = "tcp://127.0.0.1:3306";
 static const std::string USER = "root";
-static const std::string PASSWORD = "";
+static const std::string PASSWORD = "if you have a password put it here";
 
 // OANDA parameters
-static const std::string ACCOUNT_ID = "101-001-6844455-001";
-static const std::string ACCESS_TOKEN = "3481fa8462af186c57b6d6d03a37a0fd-602c4ee66c827bcb87fabddd1da5c094";
+static const std::string ACCOUNT_ID = "<Put id here>";
+static const std::string ACCESS_TOKEN = "<Put token here>";
 
 #endif
